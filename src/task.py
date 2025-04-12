f32fa2c5529f7862f796e67e431ee963f7076bdf
JuanPescoran <u20221c936@upc.edu.pe>
2025-04-12 10:03:34 -0500

    feat(task): add task type definition

---
+++ src/task.py
@@ -0,0 +1,12 @@
+from src.executable import Executable
+
+class Task(Executable):
+    def __init__(self, name: str, description: str, required_resources_name: list[str], duration_in_units: int):
+        super().__init__(name, description, required_resources_name, duration_in_units)
+
+    def execute(self) -> None:
+        if len(self._assigned_resources != len(self._required_resources_name)):
+            raise RuntimeError(f"Resources not assigned correctly for task '{self.name}'.")
+        print(f"Executing task '{self.name}': '{self._description}' (Duration: '{self._duration_in_units}' units).")
+        for resource in self._assigned_resources:
+            resource.use()
