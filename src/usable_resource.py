b6c15d4a062bb61d50618870b9ecda9a67222dbd
JuanPescoran <u20221c936@upc.edu.pe>
2025-04-12 09:39:01 -0500

    feat(usable-resource): add usable resource type definition

---
+++ src/usable_resource.py
@@ -0,0 +1,24 @@
+from src.resource import Resource, ResourceType
+
+class UsableResource(Resource):
+    def __init__(self, name:str, capacity:int):
+        super().__init__(name, ResourceType.USABLE)
+        if capacity <= 0:
+            raise ValueError(f"Capacity for '{name}' must be positive.")
+        self._capacity = capacity
+
+    def is_available_for_use(self) -> bool:
+        return self._is_available
+
+    def allocate_resource(self) -> None:
+        if not self._is_available:
+            print(f"Warning: Resource '{self.name}' is not available for use.")
+        self._is_available = False
+
+    def release(self) -> None:
+        if self._is_available:
+            print(f"Warning: Resource '{self.name}' is available for use.")
+        self._is_available = True
+
+    def use(self) -> None:
+        print(f"Using resource '{self.name}' (Capacity: '{self._capacity}' GHz).")
